d3efdf63dfe0b776effd97b2107d3ef69dbb042e
Paulina Chametka <paulinachametka@yahoo.ca>
2019-02-15 20:55:58 -0500

    code

---
+++ Alexa/options.js
@@ -0,0 +1,63 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "coder",
+            "intents": [
+                {
+                    "name": "AMAZON.FallbackIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "TestIntent",
+                    "slots": [],
+                    "samples": [
+                        "pringles",
+                        "pringles to say hello world",
+                        "pringles say hello world",
+                        "pringles hello world"
+                    ]
+                },
+                {
+                    "name": "AMAZON.PauseIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ResumeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "Penelope",
+                    "slots": [],
+                    "samples": [
+                        "a",
+                        "who's my buddy"
+                    ]
+                },
+                {
+                    "name": "HelloWorld",
+                    "slots": [],
+                    "samples": [
+                        "hello world"
+                    ]
+                }
+            ],
+            "types": []
+        }
+    }
+}
